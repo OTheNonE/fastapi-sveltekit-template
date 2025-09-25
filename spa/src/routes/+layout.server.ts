b223cdceb8e2a73a791eaa700b87687879d3f7f6
Ólavur Nón <olavnon@gmail.com>
2025-09-25 09:29:08 +0200

    remote functions are coming, and openapi needs a base url...

--- spa/src/routes/+layout.server.ts
+++ spa/src/routes/+layout.server.ts
@@ -1,11 +1,8 @@
 import { createOpenApiClient } from "$lib/openapi"
 
-export const prerender = false
-export const ssr = false
-
 export const load = async (event) => {
     const openapi = createOpenApiClient(event)
-    
+
     const { data: user, error } = await openapi.GET("/api/oauth/profile")
 
     console.log(user)
